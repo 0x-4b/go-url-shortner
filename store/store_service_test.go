384322123f732ee085d5b51a114945e9646ddab4
K <kareem.m.dev@gmail.com>
2025-07-27 16:44:52 +0000

    Unit test the store unit

--- store/store_service_test.go
+++ store/store_service_test.go
@@ -1,7 +1,17 @@
 package store
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
 var testStoreService = &StorageService{}
 
 func init() {
 	testStoreService = InitializeStorage()
+}
+
+func TestStoreInit(t *testing.T) {
+	assert.True(t, testStoreService.redisClient != nil)
 }
