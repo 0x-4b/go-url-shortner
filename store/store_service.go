690e07893189cc5472fbafe46636ffc54e73c29f
K <kareem.m.dev@gmail.com>
2025-07-27 17:29:03 +0000

    Fixed typos

--- store/store_service.go
+++ store/store_service.go
@@ -26,7 +26,7 @@ func InitializeStorage() *StorageService {
 		DB:       0,
 	})
 
-	pong, err := redisClient.ping(ctx).Result()
+	pong, err := redisClient.Ping(ctx).Result()
 	if err != nil {
 		panic(fmt.Sprintf("Error init Redis - Error: %v", err))
 	}
@@ -37,14 +37,14 @@ func InitializeStorage() *StorageService {
 }
 
 func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
-	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Error()
+	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
 	if err != nil {
 		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
 	}
 }
 
 func RetriveInitialUrl(shortUrl string) string {
-	result, err := storeService.redisClient.Get(ctx, shortUrl).Error()
+	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
 	if err != nil {
 		panic(fmt.Sprintf("Failed RetriveIntialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
 	}
