be33eb9dd17be1dbed268a87cb87a80cb2cc6fa4
K <kareem.m.dev@gmail.com>
2025-07-27 17:52:31 +0000

    Define handler subs

--- handler/handlers.go
+++ handler/handlers.go
@@ -1,1 +1,13 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func CreateShortUrl(c *gin.Context) {
+	// Implementation later
+}
+
+func HandleShortUrl(c *gin.Context) {
+	// Implementation later
+}
