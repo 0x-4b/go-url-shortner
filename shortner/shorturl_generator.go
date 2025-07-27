4ca7fad60d34650cb72b16f60fcf4249e93e8e6f
K <kareem.m.dev@gmail.com>
2025-07-27 17:38:22 +0000

    Implemented Sha256

--- shortner/shorturl_generator.go
+++ shortner/shorturl_generator.go
@@ -1,1 +1,11 @@
 package shortner
+
+import (
+	"crypto/sha256"
+)
+
+func sha2560f(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
